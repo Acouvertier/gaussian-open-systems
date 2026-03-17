cb8ec0e17b1e3d858ec55a8a4f6bd9c8f8a6cb69
Austen Couvertier <acouvertier5@gmail.com>
2026-03-17 11:08:49 -0400

    system.py -> systems.py; implemented quadratic hamiltonian and arbitrary single-environment jump operator; implemented automated multi-step unitary to thermal vacuum to generate initial conditions; created dissertation chapter 3 notebook and confirmed DFS and steady-state entanglement for aligned squeezing

--- src/gaussian_systems/initial_state.py
+++ src/gaussian_systems/initial_state.py
@@ -1,13 +1,17 @@
 import numpy as np
 import numpy.typing as npt
-from .conventions import rotation_matrix, index_list
+from .conventions import rotation_matrix, index_list, compress_mean_covariance
 
-def thermal_vacuum_covariance(n:int, nbars:npt.NDArray[np.number]) -> npt.NDArray[np.number]:
-    if n > len(nbars):
+def thermal_vacuum_covariance(n:int, nbars:npt.NDArray[np.number]=None) -> npt.NDArray[np.number]:
+    if nbars is None:
+        nbars_full = np.zeros(n)
+    elif n > len(nbars):
         nbars_full = np.append(nbars,np.zeros(int(n-len(nbars))))
     elif n < len(nbars):
         nbars_full = nbars[:n]
-    else: nbars_full = nbars
+    else: 
+        nbars_full = nbars
+    
     diagonal_elements = nbars_full + 0.5
     return np.diag(np.append(diagonal_elements, diagonal_elements))
 
@@ -30,7 +34,7 @@ def apply_1_mode_displacement(mean_vector_covariance_matrix_tuple:tuple[npt.NDAr
     return (mean_vector + x_shift_vector + p_shift_vector, covariance_matrix)
 
 def single_mode_squeeze_matrix(squeeze_magnitude:np.number, squeeze_angle:np.number) -> npt.NDArray[np.number]:
-    rot_matrix = rotation_matrix(squeeze_angle)
+    rot_matrix = rotation_matrix(squeeze_angle/2)
     return (rot_matrix @ np.diag([np.exp(-squeeze_magnitude),np.exp(squeeze_magnitude)])) @ rot_matrix.T
 
 def apply_1_mode_squeeze_unitary(mean_vector_covariance_matrix_tuple:tuple[npt.NDArray[np.number],npt.NDArray[np.number]],
@@ -84,6 +88,31 @@ def apply_2_mode_squeeze_unitary(mean_vector_covariance_matrix_tuple:tuple[npt.N
     two_squeeze_matrix = two_mode_squeezing_matrix(squeeze_magnitude,squeeze_angle)
     n_mode_squeeze_unitary = np.identity(2*n) + (selection_matrix.T) @ (two_squeeze_matrix - np.identity(4)) @ selection_matrix
     return (n_mode_squeeze_unitary @ mean_vector, (n_mode_squeeze_unitary @ covariance_matrix) @ n_mode_squeeze_unitary.T)
+
+"""
+operation_array element: [ op:str, value:Union[float, tuple[float,float]], mode_idx:Union[int,tuple[int,int]] ]
+op = "1d", "1s", "2s", "2m"
+"""
+
+def generate_initial_mean_covariance(n:int, nbars:npt.NDArray[np.number]=None, operation_tuple:tuple[list,...]=()) -> tuple[npt.NDArray[np.number],npt.NDArray[np.number]]:
+    
+    base_covariance = thermal_vacuum_covariance(n, nbars)
+    base_mean = thermal_vacuum_mean(n)
+
+    for operation in operation_tuple:
+        op, value, mode_idx = operation
+        if op.lower() == "1d":
+            base_mean, base_covariance = apply_1_mode_displacement((base_mean, base_covariance), value, mode_idx)
+        elif op.lower() == "1s":
+            base_mean, base_covariance = apply_1_mode_squeeze_unitary((base_mean, base_covariance), value, mode_idx)
+        elif op.lower() == "2s":
+            base_mean, base_covariance = apply_2_mode_squeeze_unitary((base_mean, base_covariance),value, mode_idx)
+        elif op.lower() == "2m":
+            base_mean, base_covariance = apply_2_mode_mix_unitary((base_mean, base_covariance),value, mode_idx)
+        else:
+            print(f"Invalid operation choice: {op}. Expected '1d', '1s', '2s', '2m'. Skipped.")
+
+    return compress_mean_covariance(base_mean, base_covariance)
     
     
     
---
+++ src/gaussian_systems/systems.py
@@ -0,0 +1,124 @@
+import numpy as np
+import numpy.typing as npt
+from gaussian_systems.conventions import symmetrize_matrix, index_list, symplectic_matrix, compress_mean_covariance, extract_mean_covariance, _x_subsystem, _p_subsystem
+from gaussian_systems.metrics import valid_covariance
+from typing import Callable
+
+def _self_energy(n:int, mode_id:int, frequency:float = 0.0) -> npt.NDArray[np.number]:
+    blank_matrix = np.zeros((2*n,2*n))
+    idx, idp = index_list(n, (mode_id,))
+    blank_matrix[idx, idx] = frequency
+    blank_matrix[idp, idp] = frequency
+    return blank_matrix
+
+def _xixj_term(n:int, subsystem:tuple[int,int], coupling:float) -> npt.NDArray[np.number]:
+    blank_matrix = np.zeros((2*n,2*n))
+    idx1, idx2 = _x_subsystem(n,subsystem)
+    blank_matrix[idx1,idx2] = coupling
+    blank_matrix[idx2,idx1] = coupling
+    return blank_matrix
+
+def _pipj_term(n:int,subsystem:tuple[int,int], coupling:float) -> npt.NDArray[np.number]:
+    blank_matrix = np.zeros((2*n,2*n))
+    idp1, idp2 = _p_subsystem(n,subsystem)
+    blank_matrix[idp1,idp2] = coupling
+    blank_matrix[idp2,idp1] = coupling
+    return blank_matrix
+
+def _xipj_term(n:int,subsystem:tuple[int,int], coupling:float) -> npt.NDArray[np.number]:
+    blank_matrix = np.zeros((2*n,2*n))
+    idx1, idx2, idp1, idp2 = index_list(n,subsystem)
+    blank_matrix[idx1,idp2] = coupling
+    blank_matrix[idp2,idx1] = coupling
+    return blank_matrix
+
+"""
+interaction_array element (interaction_key:str,subsystem:tuple[int,int],coupling:float)
+interation_key: xx, pp, xp
+"""
+def compile_hamiltonian(n:int,frequency_tuple:tuple[float,...]=(),interaction_array:list[tuple,...]=None) -> npt.NDArray[np.number]:
+    
+    if frequency_tuple == ():
+        unique_frequencies = np.zeros(n)
+    elif len(frequency_tuple) < n:
+        unique_frequencies = np.append(np.array(frequency_tuple), np.zeros(int(n-len(frequency_tuple)))) 
+    else:
+        unique_frequencies = np.array(frequency_tuple)[:n]
+   
+    blank_matrix = np.zeros((2*n,2*n))
+    
+    for i in range(n):
+        blank_matrix += _self_energy(n, int(i+1), unique_frequencies[i])
+    
+    if interaction_array is None:
+        interaction_array = []
+    
+    for interaction in interaction_array:
+        interaction_key, subsystem, coupling = interaction
+        if interaction_key.lower() == "xx":
+            interaction_matrix = _xixj_term(n, subsystem, coupling)
+        elif interaction_key.lower() == "pp":
+            interaction_matrix = _pipj_term(n, subsystem, coupling)
+        elif interaction_key.lower() == "xp":
+            interaction_matrix = _xipj_term(n, subsystem, coupling)
+        else:
+            print(f"compile_hamiltonian: invalid interaction key provided: {interaction_key}. Skipped.")
+            interaction_matrix = 0
+        blank_matrix += interaction_matrix
+
+    return blank_matrix
+
+"""
+decay_array element (x_p_a:str,mode_id:int,rate:float)
+x_p_a: x, p, a
+"""
+def compile_lindblad(n:int, decay_array:list[tuple,...]=None) -> npt.NDArray[np.number]:
+    decay_coefficient_array = np.zeros(2*n, dtype=complex)
+
+    if decay_array is None:
+        decay_array = []
+
+    for decay in decay_array:
+        x_p_a, mode_id, rate = decay
+        root_rate = np.sqrt(rate)
+        if x_p_a.lower() == "x":
+            idx = _x_subsystem(n,(mode_id,))[0]
+            decay_coefficient_array[idx] += root_rate
+        elif x_p_a.lower() == "p":
+            idp = _p_subsystem(n,(mode_id,))[0]
+            decay_coefficient_array[idp] += root_rate
+        elif x_p_a.lower() == "a":
+            idx, idp = index_list(n,(mode_id,))
+            decay_coefficient_array[idx] += root_rate/np.sqrt(2)
+            decay_coefficient_array[idp] += 1j*root_rate/np.sqrt(2)
+        else:
+            print(f"compile_lindblad: invalid decay operator choice: {x_p_a}. Valid choices are x, p, or a. Skipped.")
+
+    return decay_coefficient_array
+
+def generate_drift_diffusion(n:int, frequency_tuple:tuple[float,...]=(),interaction_array:list[tuple,...]=None, decay_array:list[tuple,...]=None) -> tuple[npt.NDArray[np.number],npt.NDArray[np.number]]:
+    
+    hamiltonian_matrix = compile_hamiltonian(n,frequency_tuple,interaction_array)
+    lindblad_array = compile_lindblad(n, decay_array)
+    omega_matrix = symplectic_matrix(n)
+
+    lindblad_matrix = np.outer(np.conjugate(lindblad_array),lindblad_array)
+
+    drift = omega_matrix @ (hamiltonian_matrix + np.imag(lindblad_matrix))
+    diffusion = omega_matrix @ np.real(lindblad_matrix) @ (omega_matrix.T)
+
+    return (drift, diffusion)
+
+def heisenberg_eom(n:int, frequency_tuple:tuple[float,...]=(),interaction_array:list[tuple,...]=None, decay_array:list[tuple,...]=None) -> Callable[[float,npt.NDArray[np.number]],npt.NDArray[np.number]]:
+    drift, diffusion = generate_drift_diffusion(n, frequency_tuple,interaction_array, decay_array)
+    def dxdt(t:float,mean_covariance:npt.NDArray):
+        x0, c0 = extract_mean_covariance(mean_covariance)
+        c0_sym = symmetrize_matrix(c0)
+        dx = drift @ x0
+        dc = drift @ c0_sym + c0_sym @ drift.T + diffusion
+        return compress_mean_covariance(dx,dc)
+
+    return dxdt
+
+
+    
